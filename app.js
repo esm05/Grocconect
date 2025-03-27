7929632ea01fd573f5a6710fbb6f989af3aa321a
Evan Malia <evan.malia@snhu.edu>
2025-03-27 16:27:46 -0400

    Modifications to static pages

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var homeRouter = require('./routes/home');
+var fruitRouter = require('./routes/produce');
 
 var app = express();
 
@@ -20,9 +21,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Setting up routes for the different pages 
 app.use('/', indexRouter);
 app.use('/home', homeRouter);
 app.use('/users', usersRouter);
+app.use('/produce', fruitRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
