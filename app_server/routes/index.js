9b629435a53d92235ca2d1d35af5161b46752bb2
Evan Malia <emaliasnow05@gmail.com>
2025-04-16 20:58:08 -0400

    Added redirect to admin page from the site hosted on the express server.

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -5,4 +5,11 @@ var controller = require('../controllers/main')
 /* GET landing page. */
 router.get('/', controller.main);
 
+router.post('', (req, res) => {
+    // Authentication goes here
+
+    // Redirect to angular SPA
+    res.redirect('http://localhost:4200')
+})
+
 module.exports = router;
